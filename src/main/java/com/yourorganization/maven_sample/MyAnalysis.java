bf0cd8c9ee3f1dee0f011c857a2ba794f12aad95
Danny van Bruggen <hexagonaal@gmail.com>
2018-01-28 14:05:19 +0100

    Upgrade to JSS 3.5.11

--- src/main/java/com/yourorganization/maven_sample/MyAnalysis.java
+++ src/main/java/com/yourorganization/maven_sample/MyAnalysis.java
@@ -1,10 +1,11 @@
 package com.yourorganization.maven_sample;
 
+import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.stmt.ReturnStmt;
-import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
-import com.github.javaparser.symbolsolver.model.typesystem.Type;
+import com.github.javaparser.resolution.types.ResolvedType;
+import com.github.javaparser.symbolsolver.resolution.SymbolSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
@@ -20,6 +21,8 @@ public class MyAnalysis {
         // Set up a minimal type solver that only looks at the classes used to run this sample.
         CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
         combinedTypeSolver.add(new ReflectionTypeSolver());
+        SymbolSolver symbolSolver = new SymbolSolver(combinedTypeSolver);
+        JavaParser.getStaticConfiguration().setSymbolResolver(symbolSolver);
 
         // Parse some code
         CompilationUnit cu = parse("class X { int x() { return 1 + 1.0; } }");
@@ -32,7 +35,7 @@ public class MyAnalysis {
         Expression onePlusOne = returnStatement.getExpression().get();
 
         // Find out what type the result of 1+1.0 has.
-        Type typeOfOnePlusOne = JavaParserFacade.get(combinedTypeSolver).getType(onePlusOne);
+        ResolvedType typeOfOnePlusOne = onePlusOne.calculateResolvedType();
 
         // Show that it's "double"
         System.out.println(typeOfOnePlusOne);
