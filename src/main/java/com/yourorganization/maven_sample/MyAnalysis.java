b5b3016cec2955182cbe942f26d18fe2bd409aad
Danny van Bruggen <hexagonaal@gmail.com>
2018-01-28 14:52:58 +0100

    Simplify the example even more

--- src/main/java/com/yourorganization/maven_sample/MyAnalysis.java
+++ src/main/java/com/yourorganization/maven_sample/MyAnalysis.java
@@ -2,21 +2,18 @@ package com.yourorganization.maven_sample;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.stmt.ReturnStmt;
+import com.github.javaparser.ast.expr.BinaryExpr;
 import com.github.javaparser.resolution.types.ResolvedType;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
-
-import java.io.IOException;
 
 /**
  * Some code that uses JavaSymbolSolver.
  */
 public class MyAnalysis {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // Set up a minimal type solver that only looks at the classes used to run this sample.
         CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
         combinedTypeSolver.add(new ReflectionTypeSolver());
@@ -26,19 +23,15 @@ public class MyAnalysis {
         JavaParser.getStaticConfiguration().setSymbolResolver(symbolSolver);
 
         // Parse some code
-        CompilationUnit cu = JavaParser.parse("class X { int x() { return 1 + 1.0; } }");
+        CompilationUnit cu = JavaParser.parse("class X { int x() { return 1 + 1.0 - 5; } }");
 
-        // Find the expression 1+1.0
-        ReturnStmt returnStatement = (ReturnStmt) cu
-                .getClassByName("X").get()
-                .getMethods().get(0)
-                .getBody().get().getStatements().get(0);
-        Expression onePlusOne = returnStatement.getExpression().get();
+        // Find all the calculations with two sides:
+        cu.findAll(BinaryExpr.class).forEach(be -> {
+            // Find out what type it has:
+            ResolvedType resolvedType = be.calculateResolvedType();
 
-        // Find out what type the result of 1+1.0 has.
-        ResolvedType typeOfOnePlusOne = onePlusOne.calculateResolvedType();
-
-        // Show that it's "double"
-        System.out.println(typeOfOnePlusOne);
+            // Show that it's "double" in every case:
+            System.out.println(be.toString() + " is a: " + resolvedType);
+        });
     }
 }
