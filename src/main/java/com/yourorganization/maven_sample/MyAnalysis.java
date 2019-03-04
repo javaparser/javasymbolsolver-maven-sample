1f213ec635bd8c5bcafb79e157fe9c3ed59e5231
Danny van Bruggen <hexagonaal@gmail.com>
2019-03-04 19:32:26 +0100

    Update library version

--- src/main/java/com/yourorganization/maven_sample/MyAnalysis.java
+++ src/main/java/com/yourorganization/maven_sample/MyAnalysis.java
@@ -1,6 +1,6 @@
 package com.yourorganization.maven_sample;
 
-import com.github.javaparser.JavaParser;
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.expr.BinaryExpr;
 import com.github.javaparser.resolution.types.ResolvedType;
@@ -20,10 +20,10 @@ public class MyAnalysis {
 
         // Configure JavaParser to use type resolution
         JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedTypeSolver);
-        JavaParser.getStaticConfiguration().setSymbolResolver(symbolSolver);
+        StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
 
         // Parse some code
-        CompilationUnit cu = JavaParser.parse("class X { int x() { return 1 + 1.0 - 5; } }");
+        CompilationUnit cu = StaticJavaParser.parse("class X { int x() { return 1 + 1.0 - 5; } }");
 
         // Find all the calculations with two sides:
         cu.findAll(BinaryExpr.class).forEach(be -> {
